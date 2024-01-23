30e56dd33e303eb1f2ce888e19028b21b8458ec7
XuanSon <Sonhxhe172036@fpt.edu.vn>
2024-01-23 16:58:59 +0700

    edit send email

--- src/main/java/controller/forgotController.java
+++ src/main/java/controller/forgotController.java
@@ -22,17 +22,19 @@ public class forgotController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
         EmailUtility emailUtility = new EmailUtility();
         String hostname = "smtp.gmail.com";
         int port = 587; // Use the appropriate port for your SMTP server
         String userName = "Sonhxhe172036@fpt.edu.vn";
         char[] password = "tjbg bmfk brch edxl".toCharArray();
-        String toAddress = "hson512475@gmail.com";
+        String toAddress = email;
         String subject = "Test Email";
         String message = "This is a test email.";
 
         try {
             emailUtility.sendEmail(hostname, String.valueOf(port), userName, password, toAddress, subject, message);
+            resp.sendRedirect("signin");
         } catch (Exception e) {
             System.out.println("Failed to send email: " + e.getMessage());
             e.printStackTrace();
