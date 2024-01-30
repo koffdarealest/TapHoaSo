3257214fbc1b422b1f7b099c23c28d2ab310ebaf
koffdarealest <dinhsontung153153@gmail.com>
2024-01-31 02:22:26 +0700

    add captcha

--- src/main/webapp/assets/js/captcha.js
+++ src/main/webapp/assets/js/captcha.js
@@ -2,7 +2,7 @@
 function createCaptcha() {
     document.getElementById('captcha').innerHTML = "";
     var charsArray =
-        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lengthOtp = 6;
     var captcha = [];
     for (var i = 0; i < lengthOtp; i++) {
@@ -26,12 +26,21 @@ function createCaptcha() {
     document.getElementById("captcha").appendChild(canvas);
 };
 
-function validateCaptcha(e) {
-    if (document.getElementById("captchaText").value == code) {
-        alert("Valid Captcha")
-        document.getElementById("captchaText").value =""
+function validateCaptcha() {
+    var inputCaptcha = document.getElementById("inputCaptcha").value;
+
+    if (inputCaptcha == code) {
+        document.getElementById("inputCaptcha").value = "";
+        alert("Captcha Matched");
+        return true;
     } else {
-        alert("Invalid Captcha. try Again");
-        createCaptcha();
+        document.getElementById("error").innerText = "Invalid Captcha. Please try again.";
+        alert("Captcha did not match")
+        return false;
     }
+}
+
+function resetCaptcha(event) {
+    event.preventDefault(); // Ngăn chặn hành vi mặc định của button (submit form)
+    createCaptcha(); // Gọi hàm tạo mới captcha ở đây
 }
