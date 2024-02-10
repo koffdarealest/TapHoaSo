60dc5f498ce62ecc96ecea0e310d86df01119b8f
koffdarealest <dinhsontung153153@gmail.com>
2024-02-10 22:34:49 +0700

    fix market.jsp

---
+++ src/main/java/controller/marketController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/market"})
+public class marketController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/view/market.jsp").forward(req, resp);
+    }
+}
