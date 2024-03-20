61220488b3ffa60b1daee8e676200ad5a7e43de4
Ly Quang Phuc <phuclywork2412@gmail.com>
2024-03-20 13:22:27 +0700

    add vnpay

---
+++ src/main/java/util/VnPayConstant.java
@@ -0,0 +1,32 @@
+package util;
+
+public class VnPayConstant {
+
+    public static String vnp_Version = "vnp_Version";
+    public static String vnp_Command = "vnp_Command";
+    public static String vnp_TmnCode = "vnp_TmnCode";
+    public static String vnp_Amount = "vnp_Amount";
+    public static String vnp_BankCode = "vnp_BankCode";
+    public static String vnp_CreateDate = "vnp_CreateDate";
+    public static String vnp_CurrCode = "vnp_CurrCode";
+    public static String vnp_IpAddr = "vnp_IpAddr";
+    public static String vnp_Locale = "vnp_Locale";
+    public static String vnp_OrderInfo = "vnp_OrderInfo";
+    public static String vnp_OrderType = "vnp_OrderType";
+    public static String vnp_ReturnUrl = "vnp_ReturnUrl";
+    public static String vnp_TxnRef = "vnp_TxnRef";
+    public static String vnp_SecureHash = "vnp_SecureHash";
+    public static String vnp_BankTranNo = "vnp_BankTranNo";
+    public static String vnp_CardType = "vnp_CardType";
+    public static String vnp_PayDate = "vnp_PayDate";
+    public static String vnp_TransactionNo = "vnp_TransactionNo";
+    public static String vnp_ResponseCode = "vnp_ResponseCode";
+    public static String vnp_TransactionStatus = "vnp_TransactionStatus";
+    public static String vnp_SecureHashType = "vnp_SecureHashType";
+    public static String vnp_ExpireDate = "vnp_ExpireDate";
+
+    public static String Success_Code = "00";
+    public static String Payment_Status_Pending = "0";
+    public static String Payment_Status_Success = "1";
+    public static String Payment_Status_Failed = "2";
+}
