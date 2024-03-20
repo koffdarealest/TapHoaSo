1b05551600cedfac77f7b602517b62edc07dfcdb
Ly Quang Phuc <phuclywork2412@gmail.com>
2024-03-20 13:22:31 +0700

    add vnpay

---
+++ src/main/java/controller/CreatePaymentController.java
@@ -0,0 +1,159 @@
+package controller;
+
+import dao.BillDAO;
+import dao.UserDAO;
+import dao.VnPayTransactionDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Bill;
+import model.User;
+import model.VnPayTransaction;
+import util.Config;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.eclipse.tags.shaded.org.apache.xalan.xsltc.compiler.Constants.CHARACTERS;
+
+@WebServlet(urlPatterns = {"/createpayment"})
+public class CreatePaymentController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Chuyển hướng yêu cầu GET sang phương thức doPost
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+            Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+            String vnp_Version = "2.1.0";
+            String vnp_Command = "pay";
+            String orderType = "other";
+            long amount = Long.parseLong(request.getParameter("amount")) * 100L;
+            String bankCode = request.getParameter("bankCode");
+
+            String transactionId = generateRandomString();
+            String vnp_TxnRef = transactionId.toString().replace("-", "");
+            String vnp_OrderInfo = (request.getParameter("info") != null) ? request.getParameter("info") : "Add Balance";
+            String vnp_IpAddr = Config.getIpAddress(request);
+            String vnp_TmnCode = Config.vnp_TmnCode;
+
+            Map<String, String> vnp_Params = new HashMap<>();
+            vnp_Params.put("vnp_Version", vnp_Version);
+            vnp_Params.put("vnp_Command", vnp_Command);
+            vnp_Params.put("vnp_TmnCode", Config.vnp_TmnCode);
+            vnp_Params.put("vnp_Amount", String.valueOf(amount));
+            vnp_Params.put("vnp_CurrCode", "VND");
+            vnp_Params.put("vnp_BankCode", (bankCode != null && !bankCode.isEmpty()) ? bankCode : "NCB");
+            vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+            vnp_Params.put("vnp_OrderInfo", (request.getParameter("info") != null) ? URLEncoder.encode(request.getParameter("info"), StandardCharsets.US_ASCII.toString()) : "Add Balance");
+            vnp_Params.put("vnp_OrderType", orderType);
+            String locate = request.getParameter("language");
+            vnp_Params.put("vnp_Locale", (locate != null && !locate.isEmpty()) ? locate : "vn");
+            vnp_Params.put("vnp_ReturnUrl", Config.vnp_Returnurl);
+            vnp_Params.put("vnp_IpAddr", Config.getIpAddress(request));
+            String vnp_CreateDate = formatter.format(cld.getTime());
+            vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+            cld.add(Calendar.MINUTE, 15);
+            String vnp_ExpireDate = formatter.format(cld.getTime());
+            vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+
+            List<String> fieldNames = new ArrayList<>(vnp_Params.keySet());
+            Collections.sort(fieldNames);
+            StringBuilder hashData = new StringBuilder();
+            StringBuilder query = new StringBuilder();
+            for (String fieldName : fieldNames) {
+                String fieldValue = vnp_Params.get(fieldName);
+                if (fieldValue != null && !fieldValue.isEmpty()) {
+                    hashData.append(fieldName).append('=').append(fieldValue);
+                    if (!fieldName.equals(fieldNames.get(fieldNames.size() - 1))) {
+                        hashData.append('&');
+                    }
+                }
+            }
+
+// Mã hóa chuỗi hashData
+            String vnp_SecureHash = Config.hmacSHA512(Config.vnp_HashSecret, hashData.toString());
+
+// Gán giá trị đã mã hóa vào tham số vnp_SecureHash
+            String queryUrl = query.toString() + "&vnp_SecureHash=" + URLEncoder.encode(vnp_SecureHash, StandardCharsets.US_ASCII.toString());
+            String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+
+
+//            String queryUrl = query.toString();
+//            String vnp_SecureHash = Config.hmacSHA512(Config.vnp_HashSecret, hashData.toString());
+//            queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+//            String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+
+            HttpSession session = request.getSession();
+            // Lấy giá trị của thuộc tính "username" từ session
+            String username = (String) session.getAttribute("username");
+            UserDAO userDAO = new UserDAO();
+            User user = userDAO.getUserByUsername(username);
+
+            Bill bill = new Bill();
+            bill.setId(transactionId);
+            bill.setAmount(amount);
+            bill.setType("a");
+            bill.setCommand(Short.parseShort("10"));
+//            bill.setCreateDate(new Date());
+//            bill.setExpireDate(vnp_ExpireDate);
+            bill.setPaymentStatus(request.getParameter("processing"));
+            bill.setUserId(user.getUserID());
+            BillDAO billDAO = new BillDAO();
+            billDAO.save(bill);
+
+            String transactionNo = generateRandomString();
+            VnPayTransaction transaction = new VnPayTransaction();
+            transaction.setId(transactionNo);
+            transaction.setType("a");
+            transaction.setVersion(vnp_Params.get("vnp_Version"));
+            transaction.setCommand(vnp_Params.get("vnp_Command"));
+            transaction.setAmount(new BigInteger(vnp_Params.get("vnp_Amount")));
+            transaction.setCurrentCode(vnp_Params.get("vnp_CurrCode"));
+            transaction.setBankCode(vnp_Params.get("vnp_BankCode"));
+            transaction.setLocale(vnp_Params.get("locale"));
+            transaction.setIpAddr(vnp_Params.get("vnp_IpAddr"));
+            transaction.setOrderInfo(vnp_Params.get("vnp_OrderInfo"));
+            transaction.setCreateDate(vnp_Params.get("vnp_CreateDate"));
+            transaction.setExpireDate(vnp_Params.get("vnp_ExpireDate"));
+            transaction.setSecureHash(vnp_Params.get("vnp_SecureHash"));
+            VnPayTransactionDAO transactionDAO = new VnPayTransactionDAO();
+            transactionDAO.save(transaction);
+
+            response.sendRedirect(paymentUrl);
+        } catch (Exception e) {
+            request.setAttribute("ERROR_MESSAGE", "Tạo giao dịch không thành công!");
+        }
+    }
+
+    public static String generateRandomString() {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder("6");
+        for (int i = 0; i < 6; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+
+    public static int generateSixDigitNumber() {
+        // Khởi tạo một đối tượng Random
+        Random random = new Random();
+        // Sinh ra một số ngẫu nhiên từ 100000 đến 999999 (bao gồm cả hai giá trị này)
+        int randomNumber = random.nextInt(900000) + 100000;
+
+        return randomNumber;
+    }
+}
---
+++ src/main/java/controller/VnPayReturnController.java
@@ -0,0 +1,92 @@
+package controller;
+
+import dao.BillDAO;
+import dao.TransactionDAO;
+import dao.UserDAO;
+import dao.VnPayTransactionDAO;
+import model.Bill;
+import model.VnPayTransaction;
+import util.Config;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.User;
+import util.VnPayConstant;
+import java.text.SimpleDateFormat;
+
+import static java.lang.System.out;
+
+
+@WebServlet(urlPatterns = {"/processVnpayReturn"})
+public class VnPayReturnController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String> fields = new HashMap<>();
+        for (Enumeration<String> params = req.getParameterNames(); params.hasMoreElements(); ) {
+            String fieldName = URLEncoder.encode(params.nextElement(), StandardCharsets.US_ASCII);
+            String fieldValue = URLEncoder.encode(req.getParameter(fieldName), StandardCharsets.US_ASCII);
+            if ((fieldValue != null) && (!fieldValue.isEmpty())) {
+                fields.put(fieldName, fieldValue);
+            }
+
+            String vnp_SecureHash = req.getParameter(VnPayConstant.vnp_SecureHash);
+            fields.remove(VnPayConstant.vnp_SecureHashType);
+            fields.remove(VnPayConstant.vnp_SecureHash);
+            String signValue = Config.hashAllFields(fields);
+
+            String vnpTxnRef = req.getParameter(VnPayConstant.vnp_TxnRef);
+            String vnpAmount = req.getParameter(VnPayConstant.vnp_Amount);
+            String vnpOrderInfo = req.getParameter(VnPayConstant.vnp_OrderInfo);
+            String vnpResponseCode = req.getParameter(VnPayConstant.vnp_ResponseCode);
+            String vnpTransactionNo = req.getParameter(VnPayConstant.vnp_TransactionNo);
+            String vnpBankCode = req.getParameter(VnPayConstant.vnp_BankCode);
+            String vnpPayDate = req.getParameter(VnPayConstant.vnp_PayDate);
+            String vnpTransactionStatus = req.getParameter(VnPayConstant.vnp_TransactionStatus);
+
+            req.setAttribute("VAR_TxnRef", vnpTxnRef);
+            long amount = Long.parseLong(vnpAmount) / 100L;
+            req.setAttribute("VAR_Amount", amount);
+            req.setAttribute("VAR_OrderInfo", vnpOrderInfo);
+            req.setAttribute("VAR_RespCode", vnpResponseCode);
+            req.setAttribute("VAR_TransNo", vnpTransactionNo);
+            req.setAttribute("VAR_BankCode", vnpBankCode);
+            req.setAttribute("VAR_PayDate", convertTimestamp(vnpPayDate));
+            req.setAttribute("IS_SUCCESS", false);
+            req.setAttribute("IS_SUCCESS", vnpTransactionStatus);
+
+
+            String username = (String) req.getSession().getAttribute("username");
+            UserDAO userDAO = new UserDAO();
+            User user = userDAO.getUserByUsername(username);
+            req.setAttribute("user", user);
+            req.getRequestDispatcher("/WEB-INF/view/viewVnPayResult.jsp").forward(req, resp);
+        }
+
+    }
+
+    public static String convertTimestamp(String timestamp) {
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+            Date date = inputFormat.parse(timestamp);
+
+            SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
